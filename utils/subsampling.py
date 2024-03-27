ac61b72c24ad305a6c5df9fd3c4e2a53090e567f
Tim Würger <44372393+koerper@users.noreply.github.com>
2024-03-27 18:33:42 +0000

    Add subsampling function

--- utils/subsampling.py
+++ utils/subsampling.py
@@ -4,4 +4,4 @@ def random_subsample(df, num_samples):
     np.random.seed(42)
     indices = np.random.choice(df.index, num_samples, replace=False)
     subsampled_df = df.loc[indices]
-    return subsampled_df
+    return subsampled_df 
